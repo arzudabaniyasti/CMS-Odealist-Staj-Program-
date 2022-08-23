37e42b37e70eff0ac200e5cc3a7585711f34fbcc
arzudabaniyasti <dabaniyastiarzu@gmail.com>
2022-08-23 15:41:49 +0300

    Add project files.

---
+++ CMS.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.Core.Entities;
+using System.Linq.Expressions;
+//bunu yaptığımız zaman dataaccess.concrete.entityframework.efproductdal içerisinde delete update ... tamam hazır halde
+namespace CMS.Core.DataAccess.EntityFramework
+{
+	//burada entity repository içerisindeki metotları her entity için implement etmek yerine 
+	//burada bir kez yazacağız
+	public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+	where TEntity : class, IEntity, new()
+		where TContext : DbContext, new()
+	{
+		public void Add(TEntity entity)
+		{
+			using (var context=new TContext())
+			{
+				var addedEntity=context.Entry(entity);
+				addedEntity.State = EntityState.Added;
+				context.SaveChanges();
+			}
+		}
+
+		public void Delete(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				var deleteEntity = context.Entry(entity);
+				deleteEntity.State = EntityState.Deleted;
+				context.SaveChanges();
+			}
+		}
+
+		public TEntity Get(Expression<Func<TEntity, bool>> filter)
+		{
+			using (var context = new TContext())
+			{
+				return context.Set<TEntity>().SingleOrDefault(filter);
+			}
+		}
+		public IList<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+		{
+			using (var context = new TContext())
+			{
+				//filtre yoksa tümünü filtre varsa filtreye göre getir
+				return filter == null 
+					? context.Set<TEntity>().ToList()
+					: context.Set<TEntity>().Where(filter).ToList();
+
+			}
+		}
+
+		public void Update(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				var updateEntity = context.Entry(entity);
+				updateEntity.State = EntityState.Modified;
+				context.SaveChanges();
+			}
+		}
+	}
+}
---
+++ CMS.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.Core.Entities;
+namespace CMS.Core.DataAccess
+{
+	public interface IEntityRepository <T> where T:class,IEntity,new()
+	{
+		//veritabanından istediğim tek bir nesneyi istediğim filtreye(ex:id) göre çekmek
+		//linq expression eklendi araştır
+		T Get(Expression<Func<T, bool>> filter);
+		//listeyi getir filtre yoksa tümünü getir
+		IList<T> GetList(Expression<Func<T, bool>> filter = null);
+		//gönderdiğim t'yi ekle
+		void Add(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+
+		
+	}
+}
