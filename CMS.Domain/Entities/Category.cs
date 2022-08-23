320ee4eaf18c1b4b15cd1cf9c3e2fdcaa6317c19
arzudabaniyasti <dabaniyastiarzu@gmail.com>
2022-08-24 00:22:01 +0300

    first step - category build attempt

---
+++ CMS.Domain/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.Core.Entities;
+
+
+namespace CMS.Domain.Entities
+{
+	public class Category: AuditableBaseEntity,IEntity
+	{
+
+		public string CategoryName { get; set; }
+		public bool CategoryStatus { get; set; }
+
+	}
+}
